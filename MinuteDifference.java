d0b13b8d3a36e6b4da77684c83e6dde16a92a92c
umarplusplus <umarplusplus@gmail.com>
2016-07-20 00:44:27 +0530

    Update MinuteDifference.java

--- MinuteDifference.java
+++ MinuteDifference.java
@@ -13,8 +13,8 @@ class MinuteDifference
 		String[] t2=b.split(":");
 		int min=0;
 		int h1=Integer.valueOf(t1[0]);
-		int m1=Integer.valueOf(t2[0]);
-		int h2=Integer.valueOf(t1[1]);
+		int h2=Integer.valueOf(t2[0]);
+		int m1=Integer.valueOf(t1[1]);
 		int m2=Integer.valueOf(t2[1]);
 		int sum=Math.abs((h1*60+m1)-(h2*60+m2));
 		System.out.println(sum);
