4557478a45971f6a837882949a2e8b5467a5d3a9
umarplusplus <umarplusplus@gmail.com>
2016-07-19 23:15:46 +0530

    Create SumOfN.java

---
+++ SumOfN.java
@@ -0,0 +1,17 @@
+/*@author Umar Farook
+ */
+import java.util.Scanner;
+
+class SumOfN
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int sum=0;
+		for(int i=1;i<=a;i++)
+			sum+=i;
+		System.out.print(sum);
+		in.close();
+	}
+}
