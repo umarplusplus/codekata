9a2930e24bbe7b5673c4e909d341aa32bd9b79d2
umarplusplus <umarplusplus@gmail.com>
2016-07-19 23:51:58 +0530

    Create OddBetween.java

---
+++ OddBetween.java
@@ -0,0 +1,19 @@
+/*@author Umar Farook
+ */
+import java.util.Scanner;
+
+class OddBetween
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		for(int i=a;i<=b;i++)
+		{
+			if(i%2!=0)
+				System.out.println(i);
+		}
+		in.close();
+	}
+}
